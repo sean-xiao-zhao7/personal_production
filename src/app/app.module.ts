bdc70d9891b2ab63fe180820f043077bc74d6183
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-11-20 14:06:22 -0500

    Add custom text to layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,7 +14,17 @@ import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent, LayoutComponent],
-  imports: [AppRoutingModule, BrowserModule, BrowserAnimationsModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
