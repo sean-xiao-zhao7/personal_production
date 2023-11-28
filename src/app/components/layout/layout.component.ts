ab66f1bcf28421e164fc9ba8611402d2ae5e1f44
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-11-28 18:46:05 -0500

    Add refined scroll animation for header

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -10,6 +10,9 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class LayoutComponent {
   currentPage: string = 'Home';
+  doShrink: boolean = true;
+  headerNormalHeight: number = 250;
+  headerShrunkHeight: number = 100;
 
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
@@ -41,16 +44,22 @@ export class LayoutComponent {
   onScroll(event: Event) {
     if (event.target) {
       const currentTop = (event.target as HTMLElement).scrollTop;
-      if (currentTop > 100) {
+      if (currentTop > 90) {
         const header = <HTMLElement>document.querySelector('#top-container');
-        if (header) {
-          header.style.height = '100px';
-        }
-      } else if (currentTop <= 100) {
+        const contentContainer = <HTMLElement>(
+          document.querySelector('#main-content-container')
+        );
+        header.style.height = '100px';
+        contentContainer.style.marginTop = '260px';
+        this.doShrink = false;
+      } else if (currentTop < 100) {
         const header = <HTMLElement>document.querySelector('#top-container');
-        if (header) {
-          header.style.height = '250px';
-        }
+
+        const contentContainer = <HTMLElement>(
+          document.querySelector('#main-content-container')
+        );
+        header.style.height = '250px';
+        contentContainer.style.marginTop = '250px';
       }
     }
   }
