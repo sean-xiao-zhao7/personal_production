8c33fcd095cc6ac5c217280e420bb797e2a91691
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-11-29 16:34:55 -0500

    Redirect all pages to home if first render
    Sidebar sets the header images, but direct URL access does not.
    Disallow URL access for now.

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
   currentPage: string = 'Home';
   doShrink: boolean = true;
   headerNormalHeight: number = 250;
@@ -21,7 +22,16 @@ export class LayoutComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    if (this.currentPage === 'Home') {
+      this.router.navigate(['/home']);
+    }
+  }
 
   setCurrentPage(
     newPage: string,
