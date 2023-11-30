595002e6d24d53ae1b3175d3233f3ea8077a32ab
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-11-30 14:36:48 -0500

    Add .icon-p-center styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomePageComponent } from './pages/home/home.component';
-import { LayoutComponent } from './components/layout/layout.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -12,6 +7,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './pages/home/home.component';
+import { LayoutComponent } from './components/layout/layout.component';
 import { AlgorithmsComponent } from './pages/algorithms/algorithms.component';
 import { PhotosComponent } from './pages/photos/photos.component';
 import { BlogComponent } from './pages/blog/blog.component';
