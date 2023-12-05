6f97bd27eb44fb92cbf3c4c898a576342d3c3bdb
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-12-05 11:20:11 -0500

    Add MatExpansionModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+
+// angular material
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { LayoutModule } from '@angular/cdk/layout';
+import { MatExpansionModule } from '@angular/material/expansion';
 
+// custom
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { AlgorithmsComponent } from './pages/algorithms/algorithms.component';
 import { BlogComponent } from './pages/blog/blog.component';
@@ -44,6 +47,7 @@ import { ProjectsPageComponent } from './pages/projects/projects.component';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
